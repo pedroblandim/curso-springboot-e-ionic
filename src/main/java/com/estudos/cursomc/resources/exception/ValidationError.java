a4ceef93e3880f5130da5f138b30afb25485510d
--global <pedro.blandim@gmail.com>
2020-12-17 21:13:01 -0300

    Personalizando o retorno da validaçao

---
+++ src/main/java/com/estudos/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,23 @@
+package com.estudos.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String error, Long timeStamp) {
+		super(status, error, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+}
